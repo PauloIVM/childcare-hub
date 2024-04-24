53b8e0a83eddb83c0afcc1caa972a9c6720c722b
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-04-24 09:42:57 -0300

    chore: add comments

--- packages/web/src/components/baby-record/parts/record.tsx
+++ packages/web/src/components/baby-record/parts/record.tsx
@@ -6,6 +6,11 @@ import { IFetchRecordResponse } from "../../../api/baby-record/types";
 
 // TODO: Refatorar para gerenciamento de estados com reducer...
 // TODO: Se tentar acessar algumas paginas sem ter feito o login, crasha
+// TODO: Parece estar rolando um bug onde o estado de dois records adicionados
+//       em seguida se misturam.
+// TODO: Adicionar snackbar e mostrar errors/success que venham da api...
+// TODO: Mudar o record-confirm pra ter três botões e ficar com mesmo tamanho...
+// TODO: Usar Button com endIcon no lugar de boxes em "Adicionar um novo evento"
 
 interface RecordProps {
     record: IFetchRecordResponse["records"][0];
