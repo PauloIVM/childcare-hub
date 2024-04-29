9e6daea4aa444c3ed9f02b8b8405eee2c3a5bbd5
PauloIVM <paulo.vieira.marinho@gmail.com>
2024-04-29 06:49:52 -0300

    chore: add comment

--- packages/server/src/application/gateways/email-gateway.ts
+++ packages/server/src/application/gateways/email-gateway.ts
@@ -1,3 +1,8 @@
+// TODO: Quando eu introduzir a funcionalidade de notificações, pode ser interessante permitir
+// 		 tanto o push-notifications quanto o email-notification. Daí talvez faça sentido eu mudar
+//		 essa interface para algo mais genérico, e na infra ter os dois tipos de gatways. Talvez
+//		 renomear aqui para "INotifierGateway"
+
 export interface IEmailGateway {
 	send(subject: string, message: string, to: string): Promise<string>;
 	createTemplate(title: string, text: string): string;
