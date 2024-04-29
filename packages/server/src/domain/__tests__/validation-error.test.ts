0b832cab43fecfb6e3275075a23ce4d29388d8ff
PauloIVM <paulo.vieira.marinho@gmail.com>
2024-04-29 19:19:31 -0300

    feat(validation-error): create test

---
+++ packages/server/src/domain/__tests__/validation-error.test.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from "../";
+
+describe("ValidationError", () => {
+    test("should", async () => {
+        try {
+            throw new ValidationError({
+                message: "error_message",
+                clientMessage: "error_client_message",
+                status: 1000
+            });
+        } catch (error) {
+            expect(error instanceof ValidationError).toBe(true);
+            expect(error.message).toBe("error_message");
+            expect(error.clientMessage).toBe("error_client_message");
+            expect(error.status).toBe(1000);
+        }
+    });
+});
