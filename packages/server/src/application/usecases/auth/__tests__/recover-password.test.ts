731d9f48617f25b6712f342c948be5b5e23b9eae
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-05-02 05:50:36 -0300

    feat(recover-password): create tests

--- packages/server/src/application/usecases/auth/__tests__/recover-password.test.ts
+++ packages/server/src/application/usecases/auth/__tests__/recover-password.test.ts
@@ -1,42 +1,75 @@
 import { JwtManager, User } from "@/domain";
 import { RecoverPasswordUsecase } from "../";
-import { createUserRepository, createEmailGateway } from "./orchestrator";
+import { createUserRepository } from "./orchestrator";
 
 describe("RecoverPasswordUsecase", () => {
     let token: string;
-    beforeAll(async () => {
-        const tokenGenerator = new JwtManager("secret");
+    beforeEach(async () => {
+        jest.spyOn(Date, "now").mockImplementation(() => new Date("01/05/2024, 01:17:11").getTime());
+        const tokenGenerator = new JwtManager("secret").setExpiresInMinutes(2);
         token = tokenGenerator.sign(
             await createUserRepository().findByEmail("user_1@gmail.com"),
             new Date("01/05/2024, 01:16:11")
         );
     });
-    test("should recover", async () => {
-        const sentMessages: string[] = [];
-        const userRepository = createUserRepository();
-        const usecase = new RecoverPasswordUsecase(userRepository);
-        const result = await usecase.exec("new_p@ssword", token, new Date("01/05/2024, 01:17:11"));
-        expect(sentMessages[0]).toBe("Olá, user_1. Você solicitou uma alteração de senha.");
-        expect(sentMessages.length).toBe(1);
+
+    afterEach(() => {
+        jest.resetAllMocks();
     });
 
-    // test("should not send email request to invalid user", async () => {
-    //     const sentMessages: string[] = [];
-    //     const userRepository = createUserRepository();
-    //     const emailGateway = createEmailGateway(sentMessages);
-    //     const usecase = new RequestRecoverUsecase(userRepository, emailGateway);
-    //     await expect(async () => usecase.exec("wrong@gmail.com")).rejects
-    //         .toThrow(new Error("No user with this email."));
-    //     expect(sentMessages.length).toBe(0);
-    // });
+    test("should recover", async () => {
+        const userRepository = createUserRepository();
+        const usecase = new RecoverPasswordUsecase(userRepository);
+        const userBeforeChange = await userRepository.findByEmail("user_1@gmail.com");
+        expect(userBeforeChange.password.equals("new_p@ssword")).toBe(false);
+        const result = await usecase.exec("new_p@ssword", token);
+        const userAfterChange = await userRepository.findByEmail("user_1@gmail.com");
+        expect(userAfterChange.password.equals("new_p@ssword")).toBe(true);
+        expect(result.userEmail).toBe("user_1@gmail.com");
+        expect(result.userName).toBe("user_1");
+        expect(result.token).not.toBeNull();
+    });
 
-    // test("should not crash", async () => {
-    //     const sentMessages: string[] = [];
-    //     const userRepository = createUserRepository();
-    //     const emailGateway = createEmailGateway(sentMessages);
-    //     const usecase = new RequestRecoverUsecase(userRepository, emailGateway);
-    //     await expect(async () => usecase.exec(null)).rejects
-    //         .toThrow(new Error("No user with this email."));
-    //     expect(sentMessages.length).toBe(0);
-    // });
+    test("should not crash with email changed", async () => {
+        const userRepository = createUserRepository();
+        const usecase = new RecoverPasswordUsecase(userRepository);
+        const user = await userRepository.findByEmail("user_1@gmail.com");
+        user.updateEmail("user_1_updated@gmail.com");
+        await userRepository.updateUser(user);
+        const result = await usecase.exec("new_p@ssword", token);
+        const userWithOldEmail = await userRepository.findByEmail("user_1@gmail.com");
+        const userWithNewEmail = await userRepository.findByEmail("user_1_updated@gmail.com");
+        expect(userWithOldEmail).toBe(undefined);
+        expect(userWithNewEmail.password.equals("new_p@ssword")).toBe(true);
+        expect(result.userEmail).toBe("user_1_updated@gmail.com");
+        expect(result.userName).toBe("user_1");
+        expect(result.token).not.toBeNull();
+    });
+
+    test("should not recover with expired token", async () => {
+        jest.spyOn(Date, "now").mockImplementation(() => new Date("01/05/2024, 01:19:11").getTime());
+        const userRepository = createUserRepository();
+        const usecase = new RecoverPasswordUsecase(userRepository);
+        await expect(async () => usecase.exec("new_p@ssword", token)).rejects
+            .toThrow(new Error("Expired or invalid token"));
+    });
+
+    test("should not crash with user deleted", async () => {
+        const tokenGenerator = new JwtManager("secret").setExpiresInMinutes(2);
+        const token = tokenGenerator.sign(
+            User.create("id_foo", "user_foo", "user_foo@gmail.com", "user_foo_p@ssword"),
+            new Date("01/05/2024, 01:16:11")
+        );
+        const userRepository = createUserRepository();
+        const usecase = new RecoverPasswordUsecase(userRepository);
+        await expect(async () => usecase.exec("new_p@ssword", token)).rejects
+            .toThrow(new Error("Could not find user."));
+    });
+
+    test("should not accept bad input", async () => {
+        const userRepository = createUserRepository();
+        const usecase = new RecoverPasswordUsecase(userRepository);
+        await expect(async () => usecase.exec("bad_password", token)).rejects
+            .toThrow(new Error("Invalid password."));
+    });
 });
