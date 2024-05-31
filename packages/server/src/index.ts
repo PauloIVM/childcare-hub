40e65c6f02a989c67aabf100df95f9ce2d0b0fc0
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-05-31 09:34:37 -0300

    feat: user dbs as a docker-service

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,7 @@ import * as http from "http";
 import { createConnection, ConnectionOptions } from "typeorm";
 import connectionOptions from "./ormconfig.json";
 import app from "./app";
+import { sleep } from "./utils";
 
 function logFatal(err: Error) {
     // TODO: Implementar loggers...
@@ -9,22 +10,28 @@ function logFatal(err: Error) {
     process.exit(1);
 }
 
-async function run() {
+async function run(retries = 5, delay = 2000) {
     // TODO: O "extendConnectionOptions" q o pessoal usa por cima desse cara la no MC
     // eh justamente pra sobrescrever as variaveis de producao do banco... como a senha
     // e url q esta ali no ormconfig. Depois vou ter que fazer a mesma coisa aqui; vai
     // ser bom eu usar isso tbm pra senha local do DB, nao correr risco da pessoa commitar.
-    return createConnection(connectionOptions as ConnectionOptions).then(async (connection) => {
-        const server = http.createServer(app);
-        server.listen(3001, "::", () => {
-            if (process.env.NODE_ENV !== "production") {
-                console.log(`API http server running on port ${3001}`);
-            }
+    return createConnection(connectionOptions as ConnectionOptions)
+        .then(async (connection) => {
+            const server = http.createServer(app);
+            server.listen(3001, "::", () => {
+                if (process.env.NODE_ENV !== "production") {
+                    console.log(`API http server running on port ${3001}`);
+                }
+            });
+            server.keepAliveTimeout = 61 * 1000;
+            server.headersTimeout = 62 * 1000;
+            // TODO: Replicar o gracefullShutdown...
+        })
+        .catch(async () => {
+            if (retries === 0) throw new Error("Failed to connect to mysql");
+            await sleep(delay);
+            run(retries - 1, delay);
         });
-        server.keepAliveTimeout = 61 * 1000;
-        server.headersTimeout = 62 * 1000;
-        // TODO: Replicar o gracefullShutdown...
-    });
 }
 
 run().catch(logFatal);
