6320c335622e70189289c2a74c4aed9e9d72fd2a
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-06-21 11:01:16 -0300

    chore: temporarly use prod ips

--- packages/web/src/gateways/instances.ts
+++ packages/web/src/gateways/instances.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 
 // TODO: Arrumar ENVs para urls de prod e dev...
 export const userApi = axios.create({
-    baseURL: "http://localhost:3003/user"
+    baseURL: "http://107.22.132.248:3003/user"
 });
 
 export const babyRecordApi = axios.create({
-    baseURL: "http://localhost:3001/baby-record"
+    baseURL: "http://3.83.152.25:3001/baby-record"
 });
 
 export const babyApi = axios.create({
-    baseURL: "http://localhost:3001/baby"
+    baseURL: "http://3.83.152.25:3001/baby"
 });
